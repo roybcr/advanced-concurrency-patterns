4fe26881c7510fbbf5253fff8d71faa8a718223c
Roy <roybcx@proton.me>
2022-08-05 20:31:03 +0300

    Adding Generator Pattern

---
+++ generator/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func generator() (<-chan int, <-chan string) {
+
+	ch1 := make(chan int   )
+	ch2 := make(chan string)
+
+	// the generator generates the next value from within an 
+	// infinite loop whenever a ready receiver asks for a value
+
+	go func() { for i := 0; ; i++ { ch1 <- i } }()
+	go func() {
+
+		for {
+
+			ch2 <- fmt.Sprintf(
+
+				"%s %d %d, %s, %d:%d:%d",
+				
+				time.Now().Month   (),
+				time.Now().Day     (),
+				time.Now().Year    (),
+				time.Now().Weekday (),
+				time.Now().Hour    (),
+				time.Now().Minute  (),
+				time.Now().Second  (),
+			)
+			
+		}
+
+	}()
+
+	return ch1, ch2;
+}
+
+func main() {
+
+	ch1, ch2 := generator()
+
+	for i := 0; i < 10; i++ {
+		
+		time.Sleep(time.Second * 2)
+
+		value 	  := <-ch1
+		timestamp := <-ch2
+
+		fmt.Printf("Generator value: %d | %s\n",
+		value, timestamp)
+		
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module concurrencyPatterns
+
+go 1.19
