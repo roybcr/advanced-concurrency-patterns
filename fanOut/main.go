9fe8fbf03c594bb62b0e896bd678d818e566d334
Roy <roybcx@proton.me>
2022-08-06 12:46:00 +0300

    Adding Fan-Out Pattern

--- fanOut/main.go
+++ fanOut/main.go
@@ -1,7 +1,48 @@
 package main
 
-import ( "fmt" ) 
+import "fmt"
 
-func main () { 
-	fmt.Println("fanOut")
+type T interface {
+	int32 | string
 }
+
+func main() {
+	input 	:= []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	rch   	:= makeRChan(input)
+	
+	outputA := fanOut(rch)
+	outputB := fanOut(rch)
+	outputC := fanOut(rch)
+	outputD := fanOut(rch)
+
+	for range rch {
+		select {
+			case val := <-outputA:
+			    fmt.Println("Output A got:", val)
+			case val := <-outputB:
+			    fmt.Println("Output B got:", val)
+			case val := <-outputC:
+			    fmt.Println("Output C got:", val)
+			case val := <-outputD:
+			    fmt.Println("Output D got:", val)
+		}
+	}
+}
+
+func fanOut[k T](inputChannel <-chan k) <-chan k {
+	outputChannel := make(chan k)
+	go func() {
+		defer close(outputChannel)
+		for data := range inputChannel { outputChannel <- data }
+	}()
+	return outputChannel
+}
+
+func makeRChan[k T](work []k) <-chan k {
+	inputChannel := make(chan k)
+	go func() {
+		defer close(inputChannel)
+		for _, v := range work { inputChannel <- v }	
+	}()
+	return inputChannel
+} 
