12706bb1cd0ef3c255142d6145295f3c721f3a7f
Roy <roybcx@pm.me>
2022-08-12 16:11:43 +0300

    adding concepts pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "concurrencyPatterns/pkg/primitives"
+import (
+con	"concurrencyPatterns/pkg/concepts"
+pri	"concurrencyPatterns/pkg/primitives"
+	"sync"
+)
 
-func main() {
-	primitives.RWMutex()
-	primitives.Cond()
-	primitives.KnightsTour()
+type runner []func()
+
+type Runner struct {
+	runner
 }
+
+// This function executes all Runner's operations concurrently.
+func (r *Runner) Run() {
+
+	var wg sync.WaitGroup
+	wg.Add(len((*r).runner))
+
+	func(wg *sync.WaitGroup) {
+		for _, fn := range (*r).runner {
+			go func(wg *sync.WaitGroup, fn func()) {
+				defer (*wg).Done()
+				fn()
+			}(wg, fn)
+		}
+	}(&wg)
+
+	wg.Wait()
+}
+
+var ConceptsRunner 	 = Runner{[]func(){con.LexicalConfinment}}
+var PrimitivesRunner = Runner{
+	[]func(){
+		pri.Cond,
+		pri.RWMutex,
+		pri.Broadcast,
+		pri.KnightsTour,
+	},
+}
+
+func runAll() {
+	ConceptsRunner.Run()
+	PrimitivesRunner.Run()
+}
+
+func main() { runAll() }
